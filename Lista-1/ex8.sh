cf47fb6d1d3b62635e3da3d626c7b24db2a5b26b
Carlos Melo <carlos.melo25@hotmail.com>
2020-09-10 10:25:02 -0300

    ex 8

--- Lista-1/ex8.sh
+++ Lista-1/ex8.sh
@@ -2,16 +2,11 @@
 
 echo "A substituição de variáveis ocorre quando alteramos os valores estabelecidos na variável como nesse caso."
 var=aluno
-echo "------------------------------"
 echo "var=aluno, logo seu valor é ${var}"
 var=professor
-echo "------------------------------"
-
 echo "Se alterarmos para var=professor, seu novo valor será ${var}."
-echo "------------------------------"
 
 echo "A substituição de shell é quando substituímos o comando pela sua saída, já que ele está sendo executado em uma subshell podendo ser armazenada numa variável."
-echo "------------------------------"
 raiz=$(ls -l /home)
 echo 'A expressão $(ls -l /home) é onde está sendo executado o comando ou seja no subshell, a variável raiz possui agora a saida desse comando.'
 echo ${raiz}

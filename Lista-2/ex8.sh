f155594120bbb1008fe6dc6d87a136af50a14f77
Carlos278 <carlos.melo25@hotmail.com>
2020-09-21 11:54:05 -0300

    exercícios 8 e 9

--- Lista-2/ex8.sh
+++ Lista-2/ex8.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+resultado=$(bc <<< "scale=2; $1 + $2 + $3")
+
+echo -e "O resultado da soma dos números $1,$2,$3 é ${resultado}."
--- Lista-2/ex9.sh
+++ Lista-2/ex9.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+echo "obase=10;scale=2;($1*100)/$2" | bc 
+echo "obase=10;scale=2;($2*100)/$1" | bc

771aa6ecfd087dbc49609cd6c893719dc671be72
Carlos278 <carlos.melo25@hotmail.com>
2020-09-27 09:44:13 -0300

    exercicio 2

--- Lista-3/ex2.sh
+++ Lista-3/ex2.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+arq1=$(cat $1 | wc -l)
+arq2=$(cat $2 | wc -l)
+arq3=$(cat $3 | wc -l)
+
+(( "${arq1}" > "${arq2}" )) && (( "${arq1}" > "${arq3}" )) && echo" ${1}"
+(( "${arq2}" > "${arq1}" )) && (( "${arq2}" > "${arq3}" )) && echo "${2}"
+(( "${arq3}" > "${arq1}"))  && (( "${arq3}" > "${arq1}" )) && echo "${3}"
+
+exit 0

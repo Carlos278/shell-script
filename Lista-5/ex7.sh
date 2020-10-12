d33c05e8d8e5d1273f78cc4a4877a1563dfc0fd9
Carlos278 <carlos.melo25@hotmail.com>
2020-10-12 13:33:39 -0300

    exercícios

--- Lista-5/ex7.sh
+++ Lista-5/ex7.sh
@@ -3,13 +3,5 @@ for i in *
 do
 	a=$(sudo md5sum ${i})
 
-for j in *
-do
-	b=$(sudo md5sum ${j})
-
-res=$(bc <<< "$a == $b")
-if [ res && exit 0 ] ; then
-	echo "${a} | ${b}"
-fi
-done
+test "$a" == "$a" && echo "${a} | ${a}"
 done

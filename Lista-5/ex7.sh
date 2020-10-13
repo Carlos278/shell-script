8b51c06f2564eff6fe9d679e9de8e4218bfe396e
Carlos278 <carlos.melo25@hotmail.com>
2020-10-13 18:14:07 -0300

    Exercícios

--- Lista-5/ex7.sh
+++ Lista-5/ex7.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-for i in *
-do
-	a=$(sudo md5sum ${i})
-
-test "$a" == "$a" && echo "${a} | ${a}"
+for i in *; do
+	a=$(cat $i | md5sum)
+	for j in *; do
+		b=$(cat $j | md5sum)
+		if [ "$a" == "$b" -a "$i" != "$j" ]; then
+			echo "Arquivos ${i} ${j} são iguais." 
+		fi
+	done
 done

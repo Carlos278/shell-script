4c35ef31b663be8302d54c73a07223a1e5061e95
Carlos278 <carlos.melo25@hotmail.com>
2020-11-18 22:04:49 -0300

    exercício 2

---
+++ Lista-10/ex2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "Digite o nome do arquivo html: " arquivo
+read -p "Digite o arquivo que deseja adicionar como conteúdo no html: " texto
+touch ${arquivo}.html
+
+echo "<html>
+<head>
+<meta charset='UTF-8'/>
+<title>${arquivo}</title>
+</head>
+<body>" >> ${arquivo}.html
+
+conteudo=$(sed -E ' 1d ' < ${texto})
+for i in ${conteudo}; do
+	echo "<p> $i </p>" >> ${arquivo}.html
+done
+
+echo "</body>
+</html>" >> ${arquivo}.html
+
+cat ${arquivo}.html
